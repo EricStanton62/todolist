864c42fcb0b94fce25f72fd8f077f7af0bbac590
Eric Stanton <Eric@321s-Mac-mini.fios-router.home>
2018-08-02 13:09:39 -0400

    fun

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class List(models.Model):
+    subject=models.CharField(max_length=30, unique=True)
+    item=models.CharField(max_length=100)
+    complete=forms.BooleanField(required=False)
+
+    def __str__(self):
+        return self.subject
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import List
 
+def home(request):
+    return render(request,'home.html', {'todo': todo})
 # Create your views here.
